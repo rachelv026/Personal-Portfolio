099942526c855b6e2053667256983bef1415afd4
rachelv026 <rachelvitaliano@gmail.com>
2024-05-22 16:24:07 -0400

    change favicon + background image + skills

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
 import Loader from './components/loader/loader';
 import { useEffect, useState } from 'react';
 
@@ -22,10 +23,11 @@ function App() {
   return isLoading ? (
     <Loader /> 
   ) : (
-    <>
+    <div className='content'>
       <NavBar />
       <Banner />
-    </>
+      <Skills />
+    </div>
   );
 }
 
